094639e8fd9df961d1fe693d71d97675771df79e
Atuoha <atuohainitiatives@gmail.com>
2023-07-07 10:32:37 +0100

    completion of loginController login function in Laravel

--- laravel/uconverse-backend/app/Http/Controllers/Api/LoginController.php
+++ laravel/uconverse-backend/app/Http/Controllers/Api/LoginController.php
@@ -32,7 +32,21 @@ class LoginController extends Controller{
         $result = DB::table('users')->select('avatar','type','description','token','access_token','online')->where($map)->first();
 
         if(empty($result)){
-            return ['code'=>0,'data'=>$result,'msg'=>'no user found'];
+            $validated['token'] = md5(uniqid().rand(100000,99999));
+            $validated['created_at'] = Carbon::now();
+            $validated['access_token'] = md5(uniqid().rand(1000000,999999));
+            $validated['expire_date'] = Carbon::now()->addDays(30);
+            $user_id = DB::table('users')->insertGetId($validated);
+
+            $user_result =  DB::table('users')->select('avatar','type','description','token','access_token','online')->where('id','=',$user_id)->first();
+
+            return ['code'=>0,'data'=>$user_result,'msg'=>'User has been created'];
+        }else{
+            $access_token = md5(uniqid().rand(1000000,999999));
+            $expire_date = Carbon::now()->addDays(30);
+            DB::table('users')->where($map)->update(['access_token'=> $access_token, 'expire_date'=> $expire_date]);
+            $result->access_token = $access_token;
+            return ['code'=>1,'data'=>$result,'msg'=>'User information updated'];
         }
     }
 
